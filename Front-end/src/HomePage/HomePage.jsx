1e9984ac2ffba6b5b36c4e2b00758cfc1c837152
nikingoda <ngoducanh6a01@gmail.com>
2024-06-03 02:55:48 +0700

    fix the navbar, login logout function, save login data

--- Front-end/src/HomePage/HomePage.jsx
+++ Front-end/src/HomePage/HomePage.jsx
@@ -1,51 +1,34 @@
 import "./HomePage.css";
 import Component from "./Component/Component";
-import { useNavigate } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
 
 
 const HomePage = () => {
-  const navigate = useNavigate();
-  const verifyToken = () => {
-    const currentTime = new Date().getTime() / 1000;
-    const token = localStorage.getItem('token');
-    if (!token) return false;
-    const payload = JSON.parse(atob(token.split(".")[1]));
-    if (currentTime > payload.exp) {
-      localStorage.removeItem("token");
-      return false;
-    }
-    return true;
-  }
 
-  if(verifyToken()) {
-    return (
-      <div>
-        <NavBar/>
-        <div className="main">
-        {/* <div className="search">
-        <SearchBar />
-        </div> */}
-        <Component />
-        <Component />
-        <Component />
-        <Component />
-        <Component />
-        <Component />
-        <Component />
-        <Component />
-        <Component />
-        <Component />
-        <Component />
-        <Component />
-        <Component />
-      </div>
-      </div>
-      
-    );
-  } else {
-    navigate('/login');
-  }
+  return (
+    <div>
+      <NavBar/>
+      <div className="main">
+      {/* <div className="search">
+      <SearchBar />
+      </div> */}
+      <Component />
+      <Component />
+      <Component />
+      <Component />
+      <Component />
+      <Component />
+      <Component />
+      <Component />
+      <Component />
+      <Component />
+      <Component />
+      <Component />
+      <Component />
+    </div>
+    </div>
+    
+  );
 };
 
 export default HomePage;
--- Front-end/src/NavBar/NavBar.jsx
+++ Front-end/src/NavBar/NavBar.jsx
@@ -1,38 +1,91 @@
 // import React from "react";
 import "./NavBar.css";
+import { useNavigate } from "react-router-dom";
 const NavBar = () => {
-  return (
-    <div className="all-navbar">
-      <nav className="navbar">
-        <h1 className="logo NUS">NUS</h1>
-        <h1 className="logo Rate">Rate</h1>
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Rate</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-          <li>
-            <a className="reg-log" href="#">
-              Register
-            </a>
-          </li>
-          <li>
-            <a className="reg-log" href="#">
-              Login
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
+    const navigate = useNavigate();
+    const verifyToken = () => {
+        const currentTime = new Date().getTime() / 1000;
+        const token = localStorage.getItem('token');
+        if (!token) return false;
+        const payload = JSON.parse(atob(token.split(".")[1]));
+        if (currentTime > payload.exp) {
+          localStorage.removeItem("token");
+          return false;
+        }
+        return true;
+    }
+    if(!verifyToken()) {
+        return (
+            <div className="all-navbar">
+              <nav className="navbar">
+                <h1 className="logo NUS">NUS</h1>
+                <h1 className="logo Rate">Rate</h1>
+                <ul>
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">About</a>
+                  </li>
+                  <li>
+                    <a href="#">Course</a>
+                  </li>
+                  <li>
+                    <a href="#">Professor</a>
+                  </li>
+                  <li>
+                    <a className="reg-log" href="/register">
+                      Register
+                    </a>
+                  </li>
+                  <li>
+                    <a className="reg-log" href="/login">
+                      Login
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          );
+    } else {
+        const logOut = async(event) => {
+            event.preventDefault();
+            try {
+                localStorage.removeItem("token");
+                navigate('/login');
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        return (
+            <div className="all-navbar">
+              <nav className="navbar">
+                <h1 className="logo NUS">NUS</h1>
+                <h1 className="logo Rate">Rate</h1>
+                <ul>
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">About</a>
+                  </li>
+                  <li>
+                    <a href="#">Course</a>
+                  </li>
+                  <li>
+                    <a href="#">Professor</a>
+                  </li>
+                  <li onClick={logOut}>
+                    <a className="reg-log" href="/login">
+                      Logout
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          );
+    }
+    
 };
 
 export default NavBar;
