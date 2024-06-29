3634673803b174e73088ebd5ed6cc9908ff1d2d0
Trung Bui <thetrung205@gmail.com>
2024-06-30 01:00:54 +0800

    Add IntroPage

---
+++ Front-end/src/IntroPage/IntroPage.jsx
@@ -0,0 +1,11 @@
+import intropageStyles from "./IntroPage.module.css";
+import NavBar from "../NavBar/NavBar";
+import Footer from "../HomePage/Footer/Footer";
+const IntroPage = () => {
+  return <div>
+    <NavBar/>
+    <Footer/>
+  </div>;
+};
+
+export default IntroPage;
