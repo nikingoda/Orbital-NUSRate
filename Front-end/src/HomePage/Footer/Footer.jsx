bc8559475bf08c11d11e4e26dccd5fb95498a0e0
Trung Bui <thetrung205@gmail.com>
2024-06-30 01:58:36 +0800

    Fix a part problem of footer

--- Front-end/src/HomePage/Footer/Footer.jsx
+++ Front-end/src/HomePage/Footer/Footer.jsx
@@ -3,23 +3,23 @@ import footerStyles from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={footerStyles.footer}>
-      <div className={footerStyles.container}>
+      {/* <div className={footerStyles.container}>
         <div className={footerStyles.row}>
           <div className={footerStyles["col-md-6"]}>
-            <h3>About Us</h3>
+            <p>ABOUT US</p>
             <p></p>
           </div>
           <div className={footerStyles["col-md-6"]}>
-            <h3>Contact Us</h3>
+            <h3>CONTACT US</h3>
             <ul>
-              <li>Email: </li>
-              <li>Phone: </li>
-              <li>Address: </li>
+              <li>EMAIL: </li>
+              <li>PHONE: </li>
+              <li>ADDRESS: </li>
             </ul>
           </div>
         </div>
-      </div>
-      <div className={footerStyles["footer-bottom"]}>
+      </div> */}
+      <div className={footerStyles.footerbottom}>
         <div className={footerStyles.container}>
           <div className={footerStyles.row}>
             <div
