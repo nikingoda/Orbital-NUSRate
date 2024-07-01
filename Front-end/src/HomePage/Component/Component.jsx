611bbb74ba94302cad9d63e793b12e28012e4abe
nikingoda <ngoducanh6a01@gmail.com>
2024-07-01 12:29:56 +0700

    rename variable correctly

--- Front-end/src/HomePage/Component/Component.jsx
+++ Front-end/src/HomePage/Component/Component.jsx
@@ -1,43 +1,47 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 import component from "./Component.module.css";
+import { useNavigate } from "react-router-dom";
 
 const urlRating = "http://localhost:5173/api/submitRating";
 
 function Component({ course }) {
-  const { courseCode, courseName, courseDescription } = course;
+  const navigate = useNavigate();
+  const { moduleCode, title, courseDescription } = course;
   const [rating, setRating] = useState(0);
 
-  const handleRate = async () => {
-    const ratingData = { moduleCode: courseCode, rating };
-    try {
-      const response = await fetch(urlRating, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(ratingData),
-      });
-      if (response.ok) {
-        const responseData = await response.json();
-        console.log("Rating submitted successfully:", responseData);
-        alert("Rating submitted successfully!");
-      } else {
-        console.error("Failed to submit rating:", response.statusText);
-        alert("Failed to submit rating.");
-      }
-    } catch (error) {
-      console.error("Error submitting rating: ", error);
-      alert("Error submitting rating.");
-    }
-    console.log("Rating submitted:", rating);
+  const handleRate = () => {
+    // const ratingData = { moduleCode: moduleCode, rating };
+    // try {
+    //   const response = await fetch(urlRating, {
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify(ratingData),
+    //   });
+    //   if (response.ok) {
+    //     const responseData = await response.json();
+    //     console.log("Rating submitted successfully:", responseData);
+    //     alert("Rating submitted successfully!");
+    //   } else {
+    //     console.error("Failed to submit rating:", response.statusText);
+    //     alert("Failed to submit rating.");
+    //   }
+    // } catch (error) {
+    //   console.error("Error submitting rating: ", error);
+    //   alert("Error submitting rating.");
+    // }
+    // console.log("Rating submitted:", rating);
+    navigate("/rate/" + moduleCode);
   };
   const handleReadReview = () => {
     // change to Readpage
+    navigate("/course/" + moduleCode);
   };
 
   return (
     <div className={component.coursecard}>
-      <h2 className={component.coursecode}>{courseCode}</h2>
-      <h3 className={component.coursename}>{courseName}</h3>
+      <h2 className={component.coursecode}>{moduleCode}</h2>
+      <h3 className={component.coursename}>{title}</h3>
       <div className={component.courserating}>
         <span>Course Rating: </span>
         {[1, 2, 3, 4, 5].map((star, index) => (
