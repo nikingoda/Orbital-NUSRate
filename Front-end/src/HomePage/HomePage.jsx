a800c6e14364d3b37f9b6f60bcf2a3564da4bfc4
Trung Bui <thetrung205@gmail.com>
2024-07-01 12:33:10 +0800

    Add pagination

--- Front-end/src/HomePage/HomePage.jsx
+++ Front-end/src/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import NavBar from "../NavBar/NavBar";
-import Component from "./Component/Component"
+import Component from "./Component/Component";
 import Footer from "./Footer/Footer";
 import homepageStyles from "./HomePage.module.css";
 
@@ -9,6 +9,8 @@ const urlCourses = "http://localhost:8080/api/course";
 
 const HomePage = () => {
   const [courses, setCourses] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const coursesPerPage = 15;
 
   useEffect(() => {
     const fetchCourses = async () => {
@@ -22,14 +24,31 @@ const HomePage = () => {
     fetchCourses();
   }, []);
 
+  const indexOfLastCourse = currentPage * coursesPerPage;
+  const indexOfFirstCourse = indexOfLastCourse - coursesPerPage;
+  const currentCourses = courses.slice(indexOfFirstCourse, indexOfLastCourse);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const totalPages = Math.ceil(courses.length / coursesPerPage);
+
   return (
     <div>
       <NavBar />
-      {/* <div className={homepageStyles.main}>
-        {courses.map((course, index) => (
+      <div className={homepageStyles.main}>
+        {currentCourses.map((course, index) => (
           <Component key={index} course={course} />
         ))}
-      </div> */}
+      </div>
+      <div className={homepageStyles.pagination}>
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            key={index}
+            onClick={() => paginate(index + 1)}
+            className={homepageStyles.pagebutton}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
       <Footer />
     </div>
   );
